32535b5f214e7385dab8465ee88f0efb1938ad7e
Kanta Tamura <kanta03085@gmail.com>
2021-01-23 21:07:20 +0900

    add: token kind

--- include/main.h
+++ include/main.h
@@ -1,4 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef enum {
+    TK_KEY,     // 予約語(Keyword)
+    TK_PUNCT,   // 区切り文字(Punctuators)
+    TK_IDENT,   // 名前(Identifier)
+    TK_NUM,     // 数値(Numeric literals)
+    TK_EOF,     // End-of-file markers
+} TokenKind;
+
+//
+// tokenize.c
+//
+
+// TODO: change static
 char* read_file(char* path);
