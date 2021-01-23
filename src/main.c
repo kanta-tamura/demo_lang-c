3a9b83d00bd91913bb8ac772ce0edf3b63e8e15c
Kanta Tamura <kanta03085@gmail.com>
2021-01-23 22:07:36 +0900

    fix: More easy-to-understand debug

--- src/main.c
+++ src/main.c
@@ -5,26 +5,28 @@ Token* token;
 
 void print_token() {
     Token* cur = token;
+    int i = 0;
     while (cur->kind != TK_EOF) {
         char token[64] = "";
         strncpy(token, cur->str, cur->len);
         switch (cur->kind) {
         case TK_KEY:
-            printf("%-10s:%s\n", token, "予約語");
+            printf("%3d:  %-10s:%s\n", i, token, "予約語");
             break;
         case TK_PUNCT:
-            printf("%-10s:%s\n", token, "記号");
+            printf("%3d:  %-10s:%s\n", i, token, "記号");
             break;
         case TK_IDENT:
-            printf("%-10s:%s\n", token, "名前");
+            printf("%3d:  %-10s:%s\n", i, token, "名前");
             break;
         case TK_NUM:
-            printf("%-10s:%s\n", token, "整数");
+            printf("%3d:  %-10s:%s\n", i, token, "整数");
             break;
         case TK_EOF:
             break;
         }
         cur = cur->next;
+        i++;
     }
 }
 
